8a3b41918ca66508667040b51a88e0de6421eba9
Kaio Maia <kaio.venicios2014@gmail.com>
2025-02-25 00:03:51 -0300

    At. de Aula

---
+++ aula37/39 - Operad. de comparação e Avaliação de curto circuito/index.js
@@ -0,0 +1,43 @@
+/*
+Operadores lógicos em JS
+&& -> AND -> E
+|| -> OR -> OU 
+! -> NOT -> NÂO
+informações 
+*/
+
+//                                          Avaliação de Curto-Circuito - Aula 39
+/*
+&& -> false && true -> false "O valor mesmo"
+|| -> false || true -> true "Retornará verdadeiro"
+
+FALSY
+false - Literal
+null / undefined
+NaN
+*/
+
+/* Exemplo de tipo de validação AND
+function vaifalar(){
+    return 'Oi';
+}
+
+let verifica = false;
+
+console.log(verifica && vaifalar());
+*/
+
+//console.log(0 || false || null || 'Kaio Maia' || True);
+
+
+
+/* Exemplo de tipo de validação OR
+
+
+const coruser = null; //pode ser "vermelho"
+const corpadr = coruser || 'preto';
+console.log(corpadr);
+*/
+
+
+
