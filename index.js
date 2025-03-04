2509720ecaa7f892433955167d4050352062acf0
Kaio Maia <kaio.venicios2014@gmail.com>
2025-03-04 00:42:38 -0300

    Add Inform

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ Entre 18 - 23 - Boa noite!
 //IF pode ser usado sozinho
 //Sempre tem um IF antes do ELSE; Posso ter varias ELSE IF; e somente um ELSE no final
 
-const hora = 59;
+// const hora = 59;
 
-if(hora >= 0 && hora <= 11){
-    console.log('Bom dia!');
-}else if(hora >= 12 && hora <= 17){
-    console.log('Boa tarde!');
-}else if(hora >= 18 && hora <= 23){
-    console.log('Boa noite!');
-}else{
-    console.log('Tudo bem?')
-}
+// if(hora >= 0 && hora <= 11){
+//     console.log('Bom dia!');
+// }else if(hora >= 12 && hora <= 17){
+//     console.log('Boa tarde!');
+// }else if(hora >= 18 && hora <= 23){
+//     console.log('Boa noite!');
+// }else{
+//     console.log('Tudo bem?')
+// }
 
 const tenhograma = false;
 
