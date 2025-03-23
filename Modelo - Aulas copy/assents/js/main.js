0c7fda1bd1366f3bddaffb3807ec4f4803ac4d23
Kaio Maia <kaio.venicios2014@gmail.com>
2025-03-23 17:31:31 -0300

    Aulas JS - Novis

--- Modelo - Aulas copy/assents/js/main.js
+++ Modelo - Aulas copy/assents/js/main.js
@@ -1,15 +1,47 @@
 const form = document.querySelector('#form');
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('Evento predefinido')
-    setResult('Olá Mundo')
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    console.log(peso, altura)
+    if (!peso){
+        setResult('Peso Inválido!', false);
+        return;
+    }
+
+    if (!altura){
+        setResult('Altura Inválida!', false);
+        return;
+    }
+
+    console.log('cheguei aqui...');
+
+    const imc = getImc(peso, altura);
+
 });
 
+function getImc () {
 
-function setResult(msg){
-    const resultado = document.querySelector('#resultado')
-    resultado.innerHTML = `msg da doidinha sairia dessa facel`;
 
+}
+
+function criaP () {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResult(msg, isValid){
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    
+
+    const p = criaP();
+    p.innerHTML = msg;
+    resultado.appendChild(p);
 
 }
