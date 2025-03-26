66cc22c58177e3600e3e157e4aa59da290d9f82e
Kaio Maia <kaio.venicios2014@gmail.com>
2025-03-26 00:46:42 -0300

    Atualizações da ultima aula - Salvando!

---
+++ Modelo - Aulas_Estudos_IMC/assents/js/main.js
@@ -0,0 +1,86 @@
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    console.log(peso, altura)
+    if (!peso){
+        setResult('Peso Inválido!', false);
+        return;
+    }
+
+    if (!altura){
+        setResult('Altura Inválida!', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivel = getNivelImc(imc)
+
+    const msg = `Seu IMC é ${imc} (${nivel}).`
+  
+
+setResult (msg, true);
+
+});
+
+/*
+
+Menos do que 18,5
+Entre 18,5 e 24,9
+Entre 25 e 29,9
+Entre 30 e 34,9
+Entre 35 e 39,9
+Mais do que 40
+Resultado
+Abaixo do peso
+Peso normal
+Sobrepeso
+Obesidade grau 1 Obesidade grau 2 Obesidade grau 3
+
+*/
+
+function getNivelImc (imc){
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 
+    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if(imc >= 39.9) return nivel[5];
+    if(imc >= 34.9) return nivel[4];
+    if(imc >= 29.9) return nivel[3];
+    if(imc >= 24.9) return nivel[2];
+    if(imc >= 18.5) return nivel[1];
+    if(imc < 18.5)  return nivel[0];
+}
+
+
+function getImc (peso , altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+
+}
+
+function criaP () {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResult(msg, isValid){
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    const p = criaP();
+
+    if (isValid){
+        p.classList.add('paragrafo-resultado');
+    }else{
+        p.classList.add('paragrafo-resultado_bad');
+    }
+
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+
+}
